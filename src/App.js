767d605a80544c5abe7ab3d053bc0f93664d2094
Sagar naik <sagarnaikg00@gmail.com>
2021-07-23 13:30:58 +0530

    code refiing

--- src/App.js
+++ src/App.js
@@ -3,152 +3,43 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.scss";
 import gsap from "gsap";
 
+import { slideLeft, slideRight } from "./animation";
+import { testimonials } from "./testmonials";
+
 // icons
 import { ReactComponent as LeftArrow } from "./assets/arrow-left.svg";
 import { ReactComponent as RightArrow } from "./assets/arrow-right.svg";
-// testimonials images
-import image1 from "./assets/image.jpg";
-import image2 from "./assets/image2.jpg";
-import image3 from "./assets/image3.jpg";
-
-const testimonials = [
-  {
-    name: "Julia Cameron",
-    title: "Creative Director, VISA",
-    image: image1,
-    quote:
-      "It's all good. I was amazed at the quality of the Design. We've seen amazing results already.",
-  },
-  {
-    name: "Mark Jacobs",
-    title: "Tech Lead, Google",
-    image: image2,
-    quote:
-      "The rebranding has really helped our business. Definitely worth the investment.",
-  },
-  {
-    name: "Lisa Bearings",
-    title: "Brand Coordinator, Facebook",
-    image: image3,
-    quote:
-      "The service was excellent. Absolutely wonderful! A complete redesign did it for us.",
-  },
-];
 
 const App = () => {
   let imageList = useRef(null);
   let tesimonialsList = useRef(null);
   const [state, setState] = useState({ fist: true, sec: false, third: false });
   const [pointer, setPointer] = useState(0);
+  const [disable, setDisable] = useState(false);
 
   useEffect(() => {
     gsap.to(tesimonialsList.children[0], { duration: 0, opacity: 1 });
   }, []);
 
-  // testimonilas fadding animation
-  const testimonialsFading = (current, next, misilns) => {
-    gsap
-      .timeline()
-      .to(tesimonialsList.children[current], {
-        duration: 0.3,
-        y: 10,
-        opacity: 0,
-      })
-      .from(tesimonialsList.children[next], { duration: 0.5, y: 10 })
-      .to(tesimonialsList.children[next], {
-        delay: -0.5,
-        y: 0,
-        duration: 0.5,
-        opacity: 1,
-      })
-      .to(tesimonialsList.children[misilns], { duration: 1, opacity: 0 });
-  };
-
-  // image slidr animation
-  const imageSlider = (currt, next, mesilns, distance) => {
-    const tl = gsap.timeline();
-    tl.to(imageList.children[currt], {
-      duration: 1,
-      x: distance,
-      ease: "power3.out",
-    })
-      .from(imageList.children[next], {
-        delay: -1,
-        scale: 1.4,
-        duration: 1,
-        ease: "power3.out",
-      })
-      .to(imageList.children[next], {
-        delay: -1,
-        scale: 1,
-        duration: 1,
-        x: distance,
-        ease: "power3.out",
-      })
-      .to(imageList.children[mesilns], {
-        delay: -1,
-        duration: 1,
-        x: distance,
-        ease: "power3.out",
-      });
-  };
-
-  // move right
-  const slideRight = () => {
-    //  move from image 1 to 2
-    if (state.fist === true) {
-      setState({ fist: false, sec: true, third: false });
-      let x = pointer - 300;
-      setPointer(x);
-      imageSlider(0, 1, 2, x);
-      testimonialsFading(0, 1, 2);
-    } // move from 2 to 3
-    else if (state.sec === true) {
-      setState({ fist: false, sec: false, third: true });
-      let x = pointer - 300;
-      setPointer(x);
-      imageSlider(1, 2, 0, x);
-      testimonialsFading(1, 2, 0);
-    } // move from 3 to 1
-    else {
-      setState({ fist: true, sec: false, third: false });
-      let x = 0;
-      setPointer(x);
-      imageSlider(2, 0, 1, x);
-      testimonialsFading(2, 0, 1);
-    }
-  };
-
-  const slideLeft = () => {
-    //  move from image 1 to 3
-    if (state.fist === true) {
-      setState({ fist: false, sec: false, third: true });
-      let x = -600;
-      setPointer(x);
-      imageSlider(0, 2, 1, x);
-      testimonialsFading(0, 2, 1);
-    } // move from 2 to 1
-    else if (state.sec === true) {
-      setState({ fist: true, sec: false, third: false });
-      let x = pointer + 300;
-      setPointer(x);
-      imageSlider(1, 0, 2, x);
-      testimonialsFading(1, 0, 2);
-    } // move from 3 to 2
-    else {
-      setState({ fist: false, sec: true, third: false });
-      let x = pointer + 300;
-      setPointer(x);
-      imageSlider(2, 1, 0, x);
-      testimonialsFading(2, 1, 0);
-    }
-  };
-
   return (
     <div className="testimonilas-section">
       <div className="testimonial-container">
         <div className="arrow left">
-          <button onClick={() => slideLeft()} className="icon">
+          <button
+            disabled={disable}
+            onClick={() =>
+              slideLeft(
+                state,
+                setState,
+                pointer,
+                setPointer,
+                setDisable,
+                imageList,
+                tesimonialsList
+              )
+            }
+            className="icon"
+          >
             <LeftArrow />
           </button>
         </div>
@@ -181,7 +72,21 @@ const App = () => {
           </div>
         </div>
         <div className="arrow right">
-          <button onClick={() => slideRight()} className="icon">
+          <button
+            disabled={disable}
+            onClick={() =>
+              slideRight(
+                state,
+                setState,
+                pointer,
+                setPointer,
+                setDisable,
+                imageList,
+                tesimonialsList
+              )
+            }
+            className="icon"
+          >
             <RightArrow />
           </button>
         </div>
---
+++ src/testmonials.js
@@ -0,0 +1,28 @@
+// testimonials images
+import image1 from "./assets/image.jpg";
+import image2 from "./assets/image2.jpg";
+import image3 from "./assets/image3.jpg";
+
+export const testimonials = [
+  {
+    name: "Julia Cameron",
+    title: "Creative Director, VISA",
+    image: image1,
+    quote:
+      "It's all good. I was amazed at the quality of the Design. We've seen amazing results already.",
+  },
+  {
+    name: "Mark Jacobs",
+    title: "Tech Lead, Google",
+    image: image2,
+    quote:
+      "The rebranding has really helped our business. Definitely worth the investment.",
+  },
+  {
+    name: "Lisa Bearings",
+    title: "Brand Coordinator, Facebook",
+    image: image3,
+    quote:
+      "The service was excellent. Absolutely wonderful! A complete redesign did it for us.",
+  },
+];
